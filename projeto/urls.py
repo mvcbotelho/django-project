9394e4e0664a6c76e3a41dd7aef142dcb0eeab02
Marcus Botelho <marcus.botelho@solides.com.br>
2023-01-20 09:26:51 -0300

    add recipes app

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def my_home(request):
+    return HttpResponse('Homepage')
+
+
+def my_view(request):
+    return HttpResponse('About page')
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', my_home),
+    path("about/", my_view),
 ]
